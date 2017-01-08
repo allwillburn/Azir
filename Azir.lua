4f761bf9d6ccb076e92be1ecc8f5831b4ac48779
allwillburn <livefortoday470@gmail.com>
2017-01-08 08:28:10 -0500

    Update Azir.lua

--- Azir.lua
+++ Azir.lua
@@ -30,7 +30,7 @@ GetWebResultAsync("https://raw.githubusercontent.com/allwillburn/Azir/master/Azi
 GetLevelPoints = function(unit) return GetLevel(unit) - (GetCastLevel(unit,0)+GetCastLevel(unit,1)+GetCastLevel(unit,2)+GetCastLevel(unit,3)) end
 local SetDCP, SkinChanger = 0
 
-local JaxMenu = Menu("Azir", "Azir")
+local AzirMenu = Menu("Azir", "Azir")
 
 AzirMenu:SubMenu("Combo", "Combo")
 
